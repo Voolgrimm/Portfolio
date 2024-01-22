ff905c311e72807fbf6054ad60570e6fed402a8e
jim <jimjacquesson@gmail.com>
2024-01-22 13:18:57 +0100

    V0.1 Portfolio

---
+++ script.js
@@ -0,0 +1,57 @@
+let sidenav = document.getElementById("mySidenav");
+let openBtn = document.getElementById("openBtn");
+let closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+function openNav() {
+  sidenav.classList.add("active");
+}
+
+function closeNav() {
+  sidenav.classList.remove("active");
+}
+
+let discoverBtn = document.getElementById("discoverBtn");
+let profileSection = document.querySelector(".profile");
+let skillsSection = document.querySelector(".skills");
+let projectSection = document.querySelector(".project");
+let introSection = document.getElementById("introSection");
+
+discoverBtn.onclick = function () {
+  profileSection.classList.remove("hidden");
+  skillsSection.classList.remove("hidden");
+  projectSection.classList.remove("hidden");
+  introSection.style.display = "none";
+  discoverBtn.style.display = "none";
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Le texte que vous voulez afficher
+  let textToType =
+    "Bienvenue ! Tout d'abord laisse moi me présenter je suis le professeur Kaude. \n Mon travail est de te faire découvrir le tout nouvel arrivant\n de notre monde, JIM ! Prêt ? clique sur découvrir !";
+
+  // L'élément où le texte sera affiché
+  let typedBubble = document.getElementById("typedBubble");
+
+  // Délai entre chaque lettre (en millisecondes)
+  let typingSpeed = 30;
+
+  // Fonction pour afficher le texte lettre par lettre
+  function typeText() {
+    let index = 0;
+    let intervalId = setInterval(function () {
+      typedBubble.textContent += textToType[index];
+      index++;
+
+      // Arrêtez l'animation lorsque tout le texte a été affiché
+      if (index === textToType.length) {
+        clearInterval(intervalId);
+      }
+    }, typingSpeed);
+  }
+
+  // Appelez la fonction pour commencer l'animation lorsque la page est chargée
+  typeText();
+});
